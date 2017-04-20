2441e09b1f2dbf9315537b9f35b10a1c69f0f798
Matt Rhone <matthew.rhone@outlook.com>
2017-04-20 17:37:17 -0500

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151230232412) do
+ActiveRecord::Schema.define(version: 20170420223357) do
+
+  create_table "accounts", force: :cascade do |t|
+    t.string   "name"
+    t.string   "payment_status"
+    t.datetime "created_at",     null: false
+    t.datetime "updated_at",     null: false
+  end
+
+  create_table "authors", force: :cascade do |t|
+    t.string   "name"
+    t.string   "genre"
+    t.text     "bio"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "categories", force: :cascade do |t|
     t.string   "name"
     t.datetime "created_at", null: false
@@ -23,5 +39,8 @@ ActiveRecord::Schema.define(version: 20151230232412) do
     t.text     "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.integer  "category_id"
+    t.string   "post_status"
   end
+
 end

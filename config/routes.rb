fadff6a03d4e8648ad933ccb4cc1b8066dae4243
Michael Anderson <michanders@msn.com>
2018-01-30 22:16:32 +0000

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,27 @@
 Rails.application.routes.draw do
+  resources :accounts
+  get 'accounts/new'
+
+  get 'accounts/create'
+
+  get 'accounts/edit'
+
+  get 'accounts/update'
+
+  get 'accounts/destroy'
+
+  get 'accounts/index'
+
+  get 'accounts/show'
+
+  get 'admin/dashboard'
+
+  get 'admin/stats'
+
+  get 'admin/financials'
+
+  get 'admin/settings'
+
   get 'categories/show'
 
   resources :posts, only: [:index, :show, :new, :create, :edit, :update]

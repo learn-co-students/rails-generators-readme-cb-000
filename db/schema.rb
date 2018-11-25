c226127479ea872e4243737eefc3f72fb291ea9e
Sean Altemose <sean@seanaltemose.com>
2018-11-25 23:25:29 +0000

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181120155648) do
+ActiveRecord::Schema.define(version: 20181120160027) do
+
+  create_table "accounts", force: :cascade do |t|
+    t.string   "name"
+    t.string   "payment_status"
+    t.datetime "created_at",     null: false
+    t.datetime "updated_at",     null: false
+  end
 
   create_table "authors", force: :cascade do |t|
     t.string   "name"

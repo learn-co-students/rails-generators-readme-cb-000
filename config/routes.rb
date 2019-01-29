cf5cdc513850ed296c7773ff80b730ad43ac4725
Jason Viger <jason.viger@gmail.com>
2019-01-29 01:43:00 +0000

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,21 @@
 Rails.application.routes.draw do
+  resources :accounts
+  get 'accounts/new'
+
+  get 'accounts/edit'
+
+  get 'accounts/index'
+
+  get 'accounts/show'
+
+  get 'admin/dashboard'
+
+  get 'admin/stats'
+
+  get 'admin/financials'
+
+  get 'admin/settings'
+
   get 'categories/show'
 
   resources :posts, only: [:index, :show, :new, :create, :edit, :update]

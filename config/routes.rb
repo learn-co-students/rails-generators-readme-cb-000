5cff48315d33173502b8ffb12f2b2c17f23d263b
Efua Mensah-Brown <emensahb@efbelles-laptop.lippuner.ca>
2019-11-18 11:49:27 -0600

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
+  resources :accounts
+  get 'admin/dashboard'
+
+  get 'admin/stats'
+
+  get 'admin/financials'
+
+  get 'admin/settings'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'categories/show'
 
